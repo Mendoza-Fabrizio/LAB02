90661223beab4d576b883f3e287791cee383b6bc
fabianportillodc <72370904@ucsm.edu.pe>
2021-08-31 09:47:46 -0500

    EJERCICIO PROPUESTO PRACTICA 2

---
+++ Practica 2 EjerProp/WebForm1.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Practica_2_EjerProp.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .Imagen {
+            height: 211px;
+            width: 332px;
+            margin-left: 166px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Registro de electrodomésticos" 
+            Width="479px" Height="36px" Font-Bold="True" Font-Name="Arial Black"
+            Font-Size="X-Large" ></asp:Label>
+        </div>
+        <p class="Artefactos">
+            <asp:Label ID="Label2" runat="server" Text="Artefacto"></asp:Label>&nbsp;
+            <asp:DropDownList ID="Artefactos" runat="server" Autopostback="True" OnSelectedIndexChanged="SelectedIndexChanged"></asp:DropDownList>
+        </p>
+        <p class="CodigoNombre">
+            <asp:Label ID="Label4" runat="server" Text="Codigo"></asp:Label>&nbsp;
+            <asp:TextBox ID="Codigo" runat="server" Height="24px" Width="230px"></asp:TextBox>&nbsp;
+            <asp:Label ID="Label6" runat="server" Text="Nombre"></asp:Label>&nbsp;
+            <asp:TextBox ID="Nombre" runat="server" Height="24px" Width="230px"></asp:TextBox>
+        </p>
+        <p class="DescripcionMarca">
+            <asp:Label ID="Label3" runat="server" Text="Descripción"></asp:Label>&nbsp;
+            <asp:TextBox ID="Descripcion" runat="server" Height="24px" Width="230px"></asp:TextBox>&nbsp;
+            <asp:Label ID="Label5" runat="server" Text="Marca"></asp:Label>&nbsp;
+            <asp:TextBox ID="Marca" runat="server" Height="24px" Width="230px"></asp:TextBox>
+        </p>
+        <p class="Imagen">
+            <asp:Image ID="Image1" runat="server"/>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Practica 2 EjerProp/WebForm1.aspx.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Practica_2_EjerProp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                InitArtefactos();
+            }
+        }
+
+        protected void InitArtefactos()
+        {
+            Artefactos.Items.Add("Seleccionar");
+            Artefactos.Items.Add("Televisor");
+            Artefactos.Items.Add("Refrigerador");
+            Artefactos.Items.Add("Microondas");
+            Artefactos.Items.Add("Licuadora");
+            Artefactos.Items.Add("Tostadora");
+            Artefactos.Items.Add("Cafetera");
+            Artefactos.Items.Add("Lavadora");
+        }
+
+        protected void SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Artefactos.Items[0].Selected == true)
+            {
+                Codigo.Enabled = false;
+                Codigo.Text = "";
+                Nombre.Enabled = false;
+                Nombre.Text = "";
+                Descripcion.Enabled = false;
+                Descripcion.Text = "";
+                Marca.Enabled = false;
+                Marca.Text = "";
+                Image1.ImageUrl = "";
+            }
+            if (Artefactos.Items[1].Selected == true)
+            {
+                Codigo.Enabled = false;
+                Codigo.Text = "23021";
+                Nombre.Enabled = false;
+                Nombre.Text = "Televisor";
+                Descripcion.Enabled = false;
+                Descripcion.Text = "Smart tv 60' OLED";
+                Marca.Enabled = false;
+                Marca.Text = "LG";
+                Image1.Height = 350;
+                Image1.Width = 530;
+                Image1.ImageUrl = "~/Resources/des_1_n.jpg";
+            }
+            if (Artefactos.Items[2].Selected == true)
+            {
+                Codigo.Enabled = false;
+                Codigo.Text = "23022";
+                Nombre.Enabled = false;
+                Nombre.Text = "Refrigerador";
+                Descripcion.Enabled = false;
+                Descripcion.Text = "Refrigerador con dispensador";
+                Marca.Enabled = false;
+                Marca.Text = "Samsung";
+                Image1.Height = 300;
+                Image1.Width = 300;
+                Image1.ImageUrl = "~/Resources/16754100_1.jpg";
+            }
+            if (Artefactos.Items[3].Selected == true)
+            {
+                Codigo.Enabled = false;
+                Codigo.Text = "23023";
+                Nombre.Enabled = false;
+                Nombre.Text = "Microondas";
+                Descripcion.Enabled = false;
+                Descripcion.Text = "Microondas con dorador EasyClean";
+                Marca.Enabled = false;
+                Marca.Text = "LG";
+                Image1.Height = 350;
+                Image1.Width = 370;
+                Image1.ImageUrl = "~/Resources/15768826_1.jpg";
+            }
+            if (Artefactos.Items[4].Selected == true)
+            {
+                Codigo.Enabled = false;
+                Codigo.Text = "23024";
+                Nombre.Enabled = false;
+                Nombre.Text = "Licuadora";
+                Descripcion.Enabled = false;
+                Descripcion.Text = "Licuadora 550W 2 velocidades";
+                Marca.Enabled = false;
+                Marca.Text = "Oster";
+                Image1.Height = 300;
+                Image1.Width = 300;
+                Image1.ImageUrl = "~/Resources/ac0cdf781827ac14cae9cdb4b80f97f8-product.jpg";
+            }
+            if (Artefactos.Items[5].Selected == true)
+            {
+                Codigo.Enabled = false;
+                Codigo.Text = "23025";
+                Nombre.Enabled = false;
+                Nombre.Text = "Tostadora";
+                Descripcion.Enabled = false;
+                Descripcion.Text = "Tostadora 750W Holstein";
+                Marca.Enabled = false;
+                Marca.Text = "Holstein";
+                Image1.Height = 300;
+                Image1.Width = 300;
+                Image1.ImageUrl = "~/Resources/tostadora-holstein-2-rebanadas-roja-1.jpg";
+            }
+            if (Artefactos.Items[6].Selected == true)
+            {
+                Codigo.Enabled = false;
+                Codigo.Text = "23026";
+                Nombre.Enabled = false;
+                Nombre.Text = "Cafetera";
+                Descripcion.Enabled = false;
+                Descripcion.Text = "Cafetera Oster PrimaLatte";
+                Marca.Enabled = false;
+                Marca.Text = "Oster";
+                Image1.Height = 300;
+                Image1.Width = 370;
+                Image1.ImageUrl = "~/Resources/2019148956993_2.jpg";
+            }
+            if (Artefactos.Items[7].Selected == true)
+            {
+                Codigo.Enabled = false;
+                Codigo.Text = "23027";
+                Nombre.Enabled = false;
+                Nombre.Text = "Lavadora";
+                Descripcion.Enabled = false;
+                Descripcion.Text = "Lavadora LG Miniwash con secadora";
+                Marca.Enabled = false;
+                Marca.Text = "LG";
+                Image1.Height = 350;
+                Image1.Width = 300;
+                Image1.ImageUrl = "~/Resources/14933874_1.jpg";
+            }
+        }
+    }
+}
